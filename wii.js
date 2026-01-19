f3a19057510415eeae948095b749e7df324ca21e
wtaype <wilder.taype16@gmail.com>
2026-01-18 21:56:59 -0500

    Actualizacion Principal v13.10.10

---
+++ wii.js
@@ -0,0 +1,21 @@
+
+import $ from 'jquery';
+import { savels, getls } from './widev.js';
+
+// INFORMACIÓN DEL APP 
+export let app = 'WiiBCP'
+export let lanzamiento = 2024;
+export let autor = '@wilder.taype';
+export let link = 'https://wtaype.github.io/';
+export let version = 'v13';
+
+/** Actualizar main luego esto, pero si es mucho, solo esto. (1)
+git tag v13 -m "Version v13" ; git push origin v13
+
+//  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
+git add . ; git commit -m "Actualizacion Principal v13.10.10" ; git push origin main
+
+// En caso de emergencia, para actualizar el Tag existente. (3)
+git tag -d v13 ; git tag v13 -m "Version v13 actualizada" ; git push origin v13 --force
+ ACTUALIZACION TAG[END] */ 
+
